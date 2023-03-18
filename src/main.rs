815f4f0c95ae57e6904ae0001725cf474cf1c8a9
lykevin99 <kevin.sly99@gmail.com>
2023-03-18 17:00:50 -0400

    Separated Optics from Detector and new unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,99 @@
+// use std::rand;
+
 fn main() {
     let my_optics: Optics = Optics {
-        numerical_aperture: 0.25,
+        objective: NikonObjective::Blue,
         wavelength: 500e-9,
-        pixel_size: 6.9e-6,
-        magnification: 25.0,
-        fov: (544, 728)
     };
 
+    let my_detector: Detector = Detector { 
+        pixel_size: 6.9e-6, 
+        sensor_size: (544, 728), 
+        read_noise: 15.0, 
+        dark_current: 350.0, 
+    };
+
+    println!("This is my optical system: {:#?}", my_optics);
     println!("Resolution of optics: {}", my_optics.calculate_resolution());
     println!("Ideal pixel size: {}", my_optics.calculate_ideal_pixel_size());
+
+
+    let positions: [[f32; 2]; 10] = [[10.0, 10.0]; 10];
+    let sizes: [f32; 10] = [my_detector.pixel_size * 3.0; 10];
+    let incident_photons: [u32; 10] = [1_000_000_000; 10];
+    
+    let my_particle_table: ParticleTable = ParticleTable { 
+        positions: positions, 
+        sizes: sizes, 
+        incident_photons: incident_photons 
+    };
+
+    println!("This is my particle table: {:#?}", my_particle_table);
+}
+
+#[derive(Debug)]
+pub enum NikonObjective {
+    Yellow,
+    Blue,
+    White
 }
 
 /// Represents the optics of a microscope to determine resolution,
 /// magnification, detector FOV, and detector pixel size
+#[derive(Debug)]
 struct Optics {
-    numerical_aperture: f64,
-    wavelength: f64,
-    pixel_size: f64,
-    magnification: f64,
-    fov: (u16, u16),
+    objective: NikonObjective,
+    wavelength: f32,
 }
 
-// struct ParticleTable {
-//     positions: [[f64; 2]; 10], // This should be a 2D array (# rows = # particles) (row,column position specification)
-//     sizes: [f64; 10], // vector of f64 units of pixels
-//     incident_photons: [u64; 10], // vector of u64, since photons are discrete
-// }
+#[derive(Debug)]
+struct Detector {
+    pixel_size: f32,
+    sensor_size: (u16, u16),
+    read_noise: f32,
+    dark_current: f32,
+}
+
+impl Detector {
+    fn calculate_dark_current_noise(&self, exposure: f32) -> f32 {
+        return &self.dark_current * exposure;
+    }
+}
 
 impl Optics {
-    fn calculate_resolution(&self) -> f64 {
+    fn calculate_resolution(&self) -> f32 {
         // Returns the Rayleigh resolution of the optical system
-        return 0.61 * self.wavelength / self.numerical_aperture;
+        let numerical_aperture: f32 = self.get_numerical_aperture();   
+        return 0.61 * self.wavelength / numerical_aperture; 
     }
 
-    fn calculate_ideal_pixel_size(&self) -> f64 {
-        return self.calculate_resolution() * self.magnification / 2.0;
+    fn get_mangification(&self) -> f32 {
+        match &self.objective {
+            NikonObjective::Yellow => 10.0,
+            NikonObjective::Blue => 40.0,
+            NikonObjective::White => 100.0,
+        }
     }
 
+    fn get_numerical_aperture(&self) -> f32 {
+        match &self.objective {
+            NikonObjective::Yellow => 0.25,
+            NikonObjective::Blue => 0.17,
+            NikonObjective::White => 0.17,
+        }
+    }
 
+    fn calculate_ideal_pixel_size(&self) -> f32 {
+        let magnification: f32 = self.get_mangification();
+        return self.calculate_resolution() * magnification / 2.0;
+    }
+}
+
+#[derive(Debug)]
+struct ParticleTable {
+    positions: [[f32; 2]; 10], // This should be a 2D array (# rows = # particles) (row,column position specification)
+    sizes: [f32; 10], // vector of f64 units of pixels
+    incident_photons: [u32; 10], // vector of u64, since photons are discrete
 }
 
 // fn generate_particle_table(size_image: (u16, u16)) -> ParticleTable {
@@ -61,3 +117,36 @@ impl Optics {
     
     
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn optics_calculate_resolution() {
+        let optics: Optics = Optics {
+            objective: NikonObjective::Blue,
+            wavelength: 500e-9,
+        };
+        assert_eq!(0.000_001_794_117_647, optics.calculate_resolution());
+    }
+
+    #[test]
+    fn optics_calculate_ideal_pixel_size() {
+        let optics: Optics = Optics {
+            objective: NikonObjective::Blue,
+            wavelength: 500e-9,
+        };
+        assert_eq!(0.000_035_882_352_94, optics.calculate_ideal_pixel_size());
+    }
+
+    #[test]
+    fn detector_calculate_dark_current_noise() {
+        let detector: Detector = Detector { 
+            pixel_size: 6.9e-6, 
+            sensor_size: (544, 728), 
+            read_noise: 15.0, 
+            dark_current: 350.0, 
+        };
+        assert_eq!(0.035, detector.calculate_dark_current_noise(100e-6));
+    }
+}
